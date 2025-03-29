c4ca038b642f9e30736a50255d11a86e99493695
kou <kou.matsumoto.jp@gmail.com>
2025-03-29 19:58:59 +0900

    feat: update TSNE visualization to 3D plot

--- visualize.py
+++ visualize.py
@@ -14,11 +14,11 @@ def fold_text(text: str, length: int = 50) -> str:
 fd = open('data/livedoor.json')
 docs = list(map(json.loads, fd))
 vectors = np.load('data/vectors-livedoor-ginza.npy')
-embed = TSNE(n_components=2, random_state=42).fit_transform(vectors)
+embed = TSNE(n_components=3, random_state=42).fit_transform(vectors)
 
 texts: List[str] = list(map(lambda d: fold_text(d.get("body")), docs))
 publishers: List[str] = list(map(lambda d: d.get("publisher"), docs))
 
-fig = px.scatter(x=embed[:, 0], y=embed[:, 1], color=publishers, symbol=publishers, hover_data=[texts])
+fig = px.scatter_3d(x=embed[:, 0], y=embed[:, 1], z=embed[:, 2], color=publishers, symbol=publishers, hover_data=[texts])
 
 fig.write_html('visualization.html')
